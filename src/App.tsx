375e288603785d4d6a1bcff605560f27ec4d89c1
aix3 <springqiang@gmail.com>
2022-04-09 21:48:51 +0800

    feat: react router to v5

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import './App.less'
 
-import {Route, Switch} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 
 import Layout from "./pages/layout/Layout";
 import Index from "./pages/index/Index";
 import Xml from "./pages/xml/Xml";
 import Encode from "./pages/encode/Encode";
 
+const AppRoutes = () => (
+    <Routes>
+        <Route path="/" element={<Index/>}/>
+        <Route path="/xml" element={<Xml/>}/>
+        <Route path="/encode" element={<Encode/>}/>
+    </Routes>
+)
+
 function App() {
-    return (
-        <div>
-            <Layout>
-                <Switch>
-                    <Route path="/" exact component={Index}/>
-                    <Route path="/xml" exact component={Xml}/>
-                    <Route path="/encode" exact component={Encode}/>
-                </Switch>
-            </Layout>
-        </div>
-    )
+    return <Layout><AppRoutes/></Layout>
 }
 
 export default App
